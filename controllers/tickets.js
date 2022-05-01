07c497bbc5f8573830d4dbed2d5f762a46d0a0ff
vincecustomz <vince.vuong@live.com>
2022-05-01 02:00:13 -0400

    mongoose flights lab 3 completed - v2

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -6,17 +6,32 @@ module.exports = {
     create
 }
 
-function newTicket(req, res) {
-    Flight.findById(req.params.id, function(err, flight) {
-        res.render('tickets/new', {flight})
-    })
+async function newTicket(req, res) {
+    let flight = await Flight.findById(req.params.id) 
+    res.render('tickets/new', {flight})
+    //render will look at VIEWS FOLDER/"Where you want to go" (in this case tickets/new)
 }
+// This below function is the long version of the one above
+// function newTicket(req, res) {
+//     Flight.findById(req.params.id, function(err, flight) {
+//         res.render('tickets/new', {flight})
+//         //render will look at VIEWS FOLDER/"Where you want to go" (in this case tickets/new)
+//     })
+// }
 
-function create(req, res) {
+async function create(req, res) {
     req.body.flight = req.params.id
     console.log(req.body)
 
-    Ticket.create(req.body, function (err, ticket) {
-        res.redirect(`/flights/${req.params.id}`);
-    })
+    await Ticket.create(req.body)
+    res.redirect(`/flights/${req.params.id}`);
 }
+// This below function is the long version of the one above
+// function create(req, res) {
+//     req.body.flight = req.params.id
+//     console.log(req.body)
+
+//     Ticket.create(req.body, function (err, ticket) {
+//         res.redirect(`/flights/${req.params.id}`);
+//     })
+// }
